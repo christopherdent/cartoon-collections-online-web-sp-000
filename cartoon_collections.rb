8d637e1a586193e249ac2baa46ea51bdf9a7834b
Christopher Dent <christopherdent01@gmail.com>
2019-07-29 00:27:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -28,7 +28,29 @@ def long_planeteer_calls(calls)
   calls.any? do |call|
     call.length > 4
   end 
-    
+end
+
+
+
+#####################################################################################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+def find_the_cheese(cheese_types)
+  # the array below is here to help
+  
+  
+  
+  cheese_types = ["cheddar", "gouda", "camembert"]
 end
 
 
@@ -40,7 +62,3 @@ end
 
 
 
-#def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
-#end
